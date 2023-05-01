c5b96389e8820115f7246c5d269de6998580b300
hiroki_mori3 <porizou.electro@gmail.com>
2023-05-02 05:49:00 +0900

    update

--- preprocessed_cui.sh
+++
@@ -1,8 +0,0 @@
-bag_path="$PWD/bags"
-preprocessed_path="$PWD/result"
-docker run \
-  --rm \
-  -v $bag_path:/tmp/input_bags \
-  -v $preprocessed_path:/tmp/preprocessed \
-  koide3/direct_visual_lidar_calibration:humble \
-  ros2 run direct_visual_lidar_calibration preprocess -a /tmp/input_bags /tmp/preprocessed
